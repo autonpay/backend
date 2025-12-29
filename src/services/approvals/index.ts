65a0c73d8c5bdb6e1067fa505bacfcc19176ed76
Victor Jonah <victorjonah199@gmail.com>
2025-12-29 18:01:59 +0100

    Add Approval Service Exports
    - Created a new index file for the Approval Service to streamline exports of ApprovalService, ApprovalRepository, and approval types.
    - This addition enhances the modularity and accessibility of approval-related components within the service architecture.

---
+++ src/services/approvals/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Approval Service Exports
+ */
+
+export { ApprovalService } from './approval.service';
+export { ApprovalRepository } from './approval.repository';
+export * from './approval.types';
+
